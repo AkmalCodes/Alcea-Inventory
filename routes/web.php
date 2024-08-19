5e35ab613d9830eb6a99df60414347ce4bd172fb
Akmal Hisyam <akmalhisyam97@gmail.com>
2024-08-19 18:01:15 +0800

    added auth routes from installation

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InventoryController;
 
 Route::get('/', function () {
-    return view('index');
+    return view('layouts.main');
 });
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // inventory routes
 Route::get('/inventory', [InventoryController::class, 'content'])->name('inventory.inventoryView');
+
+
+// Authentication Routes
+Auth::routes();
+
+
+
