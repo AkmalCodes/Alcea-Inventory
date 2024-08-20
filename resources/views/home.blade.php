752d6eca5e280e8c71fb15b276dc0459a0856cb6
Akmal Hisyam <akmalhisyam97@gmail.com>
2024-08-20 16:10:13 +0800

    removed home contents and added example text in welcome.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,44 @@
-@extends('layouts.app')
+@extends('layouts.main')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container-fluid">
+        <h3>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque hendrerit dignissim sem. Maecenas finibus
+            volutpat risus eget elementum. Pellentesque convallis tellus vitae velit luctus lobortis. Etiam eleifend aliquet
+            lobortis. Ut massa purus, dapibus sed laoreet at, consectetur a ante. Sed sollicitudin porta ultrices. Quisque
+            dapibus volutpat justo, eget cursus purus dignissim at. Aenean orci nunc, aliquet eu quam ac, molestie placerat
+            nibh. Praesent non eros ac quam finibus accumsan.
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+            Ut venenatis ipsum ac nisl tincidunt, sagittis elementum velit faucibus. Mauris gravida enim volutpat, cursus
+            dui in, efficitur odio. Etiam a suscipit mauris. Fusce vehicula sapien sit amet nulla tincidunt eleifend.
+            Vivamus convallis eros nec magna placerat hendrerit. Nullam elementum turpis sed purus tempus, condimentum
+            sollicitudin enim efficitur. Vestibulum consequat libero ipsum, vitae elementum massa placerat a. Etiam eget
+            risus non orci viverra pulvinar. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+            ridiculus mus. Duis lorem justo, bibendum id ultricies sollicitudin, interdum sit amet urna. Aenean maximus non
+            enim a bibendum. Morbi vehicula porta sapien, eget dictum mi porta at.
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+            Ut tempus quam interdum sagittis tincidunt. Aliquam nec imperdiet urna, eget auctor mauris. Curabitur id
+            hendrerit eros. Maecenas eu ex ante. Cras non imperdiet arcu, eu finibus sapien. Duis facilisis risus non est
+            pellentesque placerat. Donec consectetur ac libero vel elementum. Donec aliquam sagittis leo, nec aliquet urna
+            elementum ut. Quisque semper magna vel tincidunt aliquam. Donec ut nibh ut tortor finibus tristique. Donec
+            auctor urna erat, in commodo eros aliquet non. Praesent ut enim condimentum, vulputate dolor ac, rutrum dui.
+            Suspendisse urna neque, commodo ac lacinia sit amet, mattis sed lorem. Duis placerat ante nec lorem aliquet
+            eleifend.
+
+            Integer placerat lacus ex. Quisque sed imperdiet purus, vel sollicitudin mauris. Integer maximus leo quis diam
+            vulputate, a faucibus justo malesuada. Nam eu porttitor neque. Donec bibendum non lacus sed sodales. Suspendisse
+            id viverra eros. Curabitur fermentum quam ac auctor volutpat. Donec sit amet dui pellentesque, commodo enim at,
+            posuere felis. Ut commodo, libero sed lobortis faucibus, leo tortor aliquam enim, ac eleifend lacus ex eget
+            lacus. Ut eu nulla vel purus pellentesque blandit a sit amet sapien. Donec varius, felis sed porttitor varius,
+            sem ligula congue erat, sit amet faucibus dui nulla ut nulla. Nunc ut venenatis ligula. Cras laoreet congue
+            vulputate. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus
+            interdum, odio at suscipit ornare, nisi tellus varius enim, nec rutrum mi ex ut nunc. Integer malesuada lacinia
+            rutrum.
+
+            Sed pellentesque fringilla metus, non pulvinar velit feugiat rhoncus. Quisque vitae lorem vitae tortor imperdiet
+            tempor. In sed velit at ex luctus semper rhoncus vitae quam. Cras quis orci lorem. Integer non mauris
+            sollicitudin, tincidunt nulla ac, condimentum massa. Fusce efficitur nisi id ipsum suscipit, sit amet fermentum
+            mauris fermentum. Duis faucibus orci ut urna eleifend iaculis.
+        </h3>
     </div>
-</div>
 @endsection
