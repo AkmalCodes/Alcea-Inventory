ab6d9c06c1dc62079e5aba8b0b2cbe0350d95b1c
Akmal Hisyam <akmalhisyam97@gmail.com>
2024-08-26 17:38:49 +0800

    added end point for add item form to call to store item

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,15 @@ Route::get('/', function () {
 
 Route::get('/home', [HomeController::class, 'view'])->name('home');
 
-// inventory routes
+// ================== inventory routes ==================
 Route::get('/inventory', [InventoryController::class, 'view'])->name('inventory.inventoryView');
+// Route to view specific inventory
+Route::post('/inventory/{id}', [InventoryController::class, 'viewItem'])->name('inventory.viewIten');
+// Route to store the new inventory item
+Route::post('/inventory', [InventoryController::class, 'store'])->name('inventory.store');
 
 
-// Authentication Routes
+// ================== Authentication Routes ==================
 Auth::routes();
 
 
