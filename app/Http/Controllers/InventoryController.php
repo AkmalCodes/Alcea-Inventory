33e005840ec63cd6c40f200e87eae7733f3a4a36
Akmal Hisyam <akmalhisyam97@gmail.com>
2024-08-26 16:16:37 +0800

    added middleware dependency in constructor

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -5,7 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class InventoryController extends Controller
-{
+{   
+
+    public function __construct(){
+        $this->middleware('auth.guard')->only(['view']); // authenticates user before accessing inventory page  // middleware('auth') is used to authenticate the user before accessing any route that requires authentication.  // The middleware will redirect the user to the login page if they are not authenticated.  // The 'auth' middleware is defined in the Kernel.php file located in the app/Http/Kernel.php file.
+    }
+
     public function view(){ // view to display inventory content
         return view('inventory.inventoryView');
     } 
