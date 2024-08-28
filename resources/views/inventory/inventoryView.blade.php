8a60ccc283789d2d5f515e5b77b7ee468349ade9
Akmal Hisyam <akmalhisyam97@gmail.com>
2024-08-28 16:55:38 +0800

    added dynamic data

--- resources/views/inventory/inventoryView.blade.php
+++ resources/views/inventory/inventoryView.blade.php
@@ -55,80 +55,63 @@
                                 <th>Last Updated</th>
                                 <th class="d-none d-md-block">Actions</th>
                             </tr>
+                            @foreach($inventoryItems as $item)
                             <tr class="inventory-view-desktop-item">
                                 <td>
                                     <a href="">
                                         <div class="col-8 d-flex justify-content-center align-items-center">
-                                            <img src="{{ asset('images/chicken.png') }}" alt="">
+                                            <!-- Replace with dynamic image source if available -->
+                                            <img src="{{ asset('images/chicken.png') }}" alt="{{ $item->name }}">
                                         </div>
                                         <div class="col-4">
-                                            <span>Chicken</span>
+                                            <span>{{ $item->name }}</span>
                                         </div>
                                     </a>
                                 </td>
                                 <td>
                                     <ul>
-                                        <li>
-                                            <p>Supplier:</p>
-                                        </li>
-                                        <li>
-                                            <p>Supplier name</p>
-                                        </li>
-                                        <li>
-                                            <p>Quantity:</p>
-                                        </li>
-                                        <li>
-                                            <p>5 packets</p>
-                                        </li>
+                                        <li><p>Supplier:</p></li>
+                                        <li><p>{{ $item->supplier_name }}</p></li>
+                                        <li><p>Quantity:</p></li>
+                                        <li><p>{{ $item->quantity }} {{ $item->unit }}</p></li>
                                     </ul>
                                 </td>
-                                <td>10</td>
-                                <td>10</td>
+                                <td>{{ $item->quantity }}</td>
+                                <td>{{ $item->updated_at->format('d/m/Y') }}</td>
                                 <td>
-                                    <div
-                                        class="container-fluid m-0 p-0 d-flex justify-content-center align-items-center">
-                                        <button
-                                                class="col-4 mt-1 border-0 d-flex justify-content-center align-items-center"
-                                                style="background-color: transparent;">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                                    fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
-                                                    <path
-                                                        d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
-                                                </svg>
-                                            </button>
-                                            <button
-                                                class="col-4 mt-1 border-0 d-flex justify-content-center align-items-center"
-                                                style="background-color: transparent;">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                                    fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                                                    <path
-                                                        d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-                                                </svg>
-                                            </button>
-                                            <button
-                                                class="col-4 mt-1 border-0 d-flex justify-content-center align-items-center"
-                                                style="background-color: transparent;">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                                    fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                                    <path
-                                                        d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                                                    <path fill-rule="evenodd"
-                                                        d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
-                                                </svg>
-                                            </button>
+                                    <div class="container-fluid m-0 p-0 d-flex justify-content-center align-items-center">
+                                        <!-- Actions such as edit, delete, etc. -->
+                                        <button class="col-4 mt-1 border-0 d-flex justify-content-center align-items-center" style="background-color: transparent;">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+                                            </svg>
+                                        </button>
+                                        <button class="col-4 mt-1 border-0 d-flex justify-content-center align-items-center" style="background-color: transparent;">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                                            </svg>
+                                        </button>
+                                        <button class="col-4 mt-1 border-0 d-flex justify-content-center align-items-center" style="background-color: transparent;">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                                            </svg>
+                                        </button>
                                     </div>
                                 </td>
                             </tr>
+                            @endforeach
                         </table>
-                    </div>
-                    <div
-                        class="inventory-view-mobile container-fluid h-100 d-flex d-md-none flex-column px-0 px-md-auto">
+                    </div>                  
+                    {{--------------------------- BELOW IS MOBILE VIEW ---------------------------}}
+                    <div class="inventory-view-mobile container-fluid h-100 d-flex d-md-none flex-column px-0 px-md-auto">
+                        @foreach($inventoryItems as $item)
                         <div class="inventory-view-mobile-item">
                             <div class="top-inventory-view d-flex flex-row w-100 align-items-center">
                                 <div class="row">
                                     <div
                                         class="left col-4 d-flex flex-column justify-content-center align-items-center ps-3">
-                                        <a href="#"><img src="{{ asset('images/chicken.png') }}" alt=""></a>
+                                        <a href="#"><img src="{{ asset('images/chicken.png') }}" alt="{{ $item->name }}"></a>
                                         <div class="row d-flex justify-content-around flex-row w-100">
                                             <button
                                                 class="col-4 mt-1 border-0 d-flex justify-content-center align-items-center"
@@ -167,30 +150,31 @@
                                                 <p>Ingredient:</p>
                                             </li>
                                             <li>
-                                                <p>Ingredient name</p>
+                                                <p>{{$item->name}}</p>
                                             </li>
                                             <li>
                                                 <p>Supplier:</p>
                                             </li>
                                             <li>
-                                                <p>Supplier name</p>
+                                                <p>{{ $item->supplier_name }}</p>
                                             </li>
                                             <li>
                                                 <p>Quantity:</p>
                                             </li>
                                             <li>
-                                                <p>5 packets</p>
+                                                <p>{{ $item->quantity }}</p>
                                             </li>
                                         </ul>
                                     </div>
                                 </div>
                             </div>
                             <div class="bottom-inventory-view d-flex flex-row w-100 align-items-center">
-                                <div class="left col-4">Frozen</div>
-                                <div class="right col-4">19/07/24</div>
+                                <div class="left col-4">{{ $item->category }}</div>
+                                <div class="right col-4">{{ $item->updated_at }}</div>
                                 <div class="right col-4">Stocked</div>
                             </div>
                         </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
