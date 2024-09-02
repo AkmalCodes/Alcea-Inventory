340f9dc252bb7c829779b60356b82b9a13c804cb
Akmal Hisyam <akmalhisyam97@gmail.com>
2024-09-02 17:54:29 +0800

    update -- added routes for destroy and delete inventory/items

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,10 @@ Route::get('/inventory', [InventoryController::class, 'view'])->name('inventory.
 Route::get('/inventory/{id}', [InventoryController::class, 'viewItem'])->name('inventory.inventory_viewitem');
 // Route to store the new inventory item
 Route::post('/inventory', [InventoryController::class, 'store'])->name('inventory.store');
-
+// Route to delete selected item
+Route::delete('/inventory/delete/{id}', [InventoryController::class, 'destroy'])->name('inventory.destroy');
+// Route to delete selected item
+Route::update('/inventory/update/{id}', [InventoryController::class, 'update'])->name('inventory.update');
 
 // ================== Authentication Routes ==================
 Auth::routes();
