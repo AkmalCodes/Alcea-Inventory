c626157fdd75d796778017c106108e35465791fd
Akmal Hisyam <akmalhisyam97@gmail.com>
2024-09-06 12:50:26 +0800

    update -- deleted files

--- resources/views/welcome.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.main')
-@section('Welcome', 'Welcome Home')
-@section('content')
-    <div class="container d-flex justify-content-center align-items-center mt-4">
-        
-    </div>
-@endsection
