259780c050b4140eb93d302bf8584c3f69cf3909
Akmal Hisyam <akmalhisyam97@gmail.com>
2024-09-09 15:35:50 +0800

    update -- added wherecolumn instead of where statementt to retrieve the correct data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Inventory;
+use App\Models\InventoryAction;
 
 class DashboardController extends Controller
 {
@@ -13,8 +14,7 @@ class DashboardController extends Controller
 
     public function view(){ // function to get relavent information regarding inventory status
         $totalItems = Inventory::count();
-        $lowStockItems = Inventory::where('quantity', '<', 'reorder_level')->count(); // Low stock
-        $recentUpdates = Inventory::where('updated_at', '>=', now()->subDays(7))->count(); // Items updated within the last 7 days
-        return view('dashboard.dashboard', compact('totalItems', 'lowStockItems', 'recentUpdates'));
-    }
+        $lowStockItems = Inventory::whereColumn('quantity', '<', 'reorder_level')->get();
+        $recentUpdatedItems = InventoryAction::with('inventory')->where('updated_at', '>=', now()->subDays(7))->get();
+        return view('dashboard.dashboard', compact('totalItems', 'lowStockItems', 'recentUpdatedItems'));    }
 }
