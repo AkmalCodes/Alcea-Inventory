5ef0a579013de7bc6986a130a59e91670c74c59d
Akmal Hisyam <akmalhisyam97@gmail.com>
2024-09-10 11:12:12 +0800

    update -- added new position to fixed and set location to be bottom right

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,8 @@
-<div id="myToast" class="toast align-items-center text-bg-primary border-0" role="alert" aria-live="assertive"
+<div id="myToast" class="toast m-3 align-items-center position-fixed bottom-0 end-0 text-bg-primary border-0" role="alert" aria-live="assertive"
     aria-atomic="true">
     <div class="d-flex">
         <div class="toast-body">
+            test
         </div>
         <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"
             aria-label="Close"></button>
