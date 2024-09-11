b4a432af25c2cde7cf8ca2c442b8543c357eb4e9
Akmal Hisyam <akmalhisyam97@gmail.com>
2024-09-11 14:39:54 +0800

    update -- added new class names for anchor tags 'page-link-custom'

--- resources/views/dashboard/partials/recentupdateitems_pagination.blade.php
+++ resources/views/dashboard/partials/recentupdateitems_pagination.blade.php
@@ -2,17 +2,17 @@
     <nav aria-label="Page navigation example">
         <ul class="pagination">
             <li class="page-item">
-                <a class="page-link" href="{{ $recentUpdatedItems->previousPageUrl() }}" aria-label="Previous">
+                <a class="page-link-custom" href="{{ $recentUpdatedItems->previousPageUrl() }}" aria-label="Previous">
                     <span aria-hidden="true">&laquo;</span>
                 </a>
             </li>
             @for ($i = 1; $i <= $recentUpdatedItems->lastPage(); $i++)
                 <li class="page-item {{ $i == $recentUpdatedItems->currentPage() ? 'active' : '' }}">
-                    <a class="page-link" href="{{ $recentUpdatedItems->url($i) }}">{{ $i }}</a>
+                    <a class="page-link-custom" href="{{ $recentUpdatedItems->url($i) }}">{{ $i }}</a>
                 </li>
             @endfor
             <li class="page-item">
-                <a class="page-link" href="{{ $recentUpdatedItems->nextPageUrl() }}" aria-label="Next">
+                <a class="page-link-custom" href="{{ $recentUpdatedItems->nextPageUrl() }}" aria-label="Next">
                     <span aria-hidden="true">&raquo;</span>
                 </a>
             </li>
