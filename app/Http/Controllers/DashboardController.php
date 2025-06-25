a3d6305271a07c355b86c1da7d02845c5b0cead3
Akmal Hisyam <akmalhisyam97@gmail.com>
2025-06-26 04:30:24 +0800

    updated view to use forecast controller to show data on dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,40 +5,33 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Inventory;
 use App\Models\InventoryAction;
+use App\Http\Controllers\ForecastController;
 
 class DashboardController extends Controller
 {
-    public function __construct(){
-        $this->middleware('auth.guard');
-    }
-
     public function view(Request $request)
     {
+        $forecastController = new ForecastController();
+        $forecastedItems = collect($forecastController->getForecastFromAPI());
+
+        // your existing variables
         $totalItems = Inventory::count();
         $lowStockItems = Inventory::whereColumn('quantity', '<', 'reorder_level')
             ->orderBy('created_at', 'desc')
-            ->paginate(15 ); // now paginated
-        $lowStockItemsCount = Inventory::whereColumn('quantity', '<', 'reorder_level')->count();
+            ->paginate(15);
+        $lowStockItemsCount = $lowStockItems->total();
         $recentUpdatedItems = InventoryAction::where('updated_at', '>=', now()->subDays(7))
             ->with('inventory', function ($query) {
-                $query->withTrashed(); // retrieves data that has been soft deleted or in other words delted_at <> NULL
+                $query->withTrashed();
             })->paginate(7);
 
-        // If the request is an AJAX call, return the partial view for pagination
-        if ($request->ajax()) {
-            $recentUpdatedItemsPagination = view('dashboard.partials.recentupdateitems_pagination', compact('recentUpdatedItems'))->render();
-            $lowStockItemsPagination = view('dashboard.partials.lowstockitems_pagination', compact('lowStockItems'))->render();
-        
-            return response()->json([
-                'recent_update_items' => $recentUpdatedItems->items(),
-                'pagination_recentupdateitems' => $recentUpdatedItemsPagination,
-                'low_stock_items' => $lowStockItems->items(),
-                'pagination_lowstockitems' => $lowStockItemsPagination,
-            ]);
-        }
-        
-
-        return view('dashboard.dashboard', compact('totalItems', 'lowStockItems', 'recentUpdatedItems','lowStockItemsCount'));
+        return view('dashboard.dashboard', compact(
+            'totalItems',
+            'lowStockItems',
+            'lowStockItemsCount',
+            'recentUpdatedItems',
+            'forecastedItems' // pass to view
+        ));
     }
-
 }
+
